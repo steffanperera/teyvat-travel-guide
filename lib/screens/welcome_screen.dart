c818bff58252422a7858c83a0a9735c3133dd390
steffanperera <steffanperera1997@gmail.com>
2023-02-10 03:16:23 +0530

    update

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      child: Center(
+          child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          CupertinoButton.filled(
+            child: const Text("ane"),
+            onPressed: () {},
+          )
+        ],
+      )),
+    );
+  }
+}
