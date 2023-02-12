bd94cdfe3f1b4d7c368d876e97cfdcb9d214ba65
steffanperera <steffanperera1997@gmail.com>
2023-02-13 03:59:35 +0530

    update

--- lib/screens/testscreen.dart
+++ lib/screens/testscreen.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:teyvat_travel_guide/config/size_config.dart';
 
 class TestScreen extends StatelessWidget {
   const TestScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("data"),
+    SizeConfig().init(context);
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Center(
+          child: Text('mmm...'),
+        ),
+      ),
     );
   }
 }
