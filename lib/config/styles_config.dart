d92d3e9f0c1e8187dbe66db1b5c990a097b6f64b
steffanperera <steffanperera1997@gmail.com>
2023-02-13 03:52:55 +0530

    app styles

---
+++ lib/config/styles_config.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// fonts
+final kMontserratSemiBold = GoogleFonts.montserrat(
+  fontWeight: FontWeight.w600,
+);
+final kMontserratMedium = GoogleFonts.montserrat(
+  fontWeight: FontWeight.w500,
+);
+final kMontserratRegular = GoogleFonts.montserrat(
+  fontWeight: FontWeight.w400,
+);
+final kMontserratLight = GoogleFonts.montserrat(
+  fontWeight: FontWeight.w300,
+);
+
+// colors
+const kColorGold = '#D3BC8E';
+const kColorBlack = '#1E1E1E';
+const kColorWhite = '#FFFFFF';
