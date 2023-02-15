b91211b74eb2694b10c8efa4ed2dc91bc21c430f
steffanperera <steffanperera1997@gmail.com>
2023-02-16 01:43:30 +0530

    update

--- lib/config/styles_config.dart
+++
@@ -1,6 +0,0 @@
-// colors
-const kBlue = '#5F76FF';
-const kBlack = '#242424';
-const kWhite = '#FFFFFF';
-const kGrey = '#2F2F2F';
-const kLightGrey = '#717274';
--- lib/models/test_model.dart
+++
@@ -1,6 +0,0 @@
-class TestModle {
-  String? data_title;
-  String? data_image_url;
-
-  TestModle(this.data_title, this.data_image_url);
-}
