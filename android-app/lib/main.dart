b3b2f4853087a1a42860e1d7e92b4815fe5d8868
steffanperera <steffanperera1997@gmail.com>
2023-02-16 23:16:09 +0530

    update

--- android-app/lib/main.dart
+++ android-app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:teyvat_travel_guide/pages/sign_in.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,13 +11,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      // title
       title: 'Teyvat Travel Guide',
-      home: Scaffold(
-        body: Center(
-          child: Text('data'),
-        ),
+
+      // theme
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        fontFamily: 'Inter',
       ),
+
+      // root
+      home: const SignIn(),
     );
   }
 }
